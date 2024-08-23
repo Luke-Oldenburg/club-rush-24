b5b5e8be43248c0e847e706181014aca0fa41036
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2024-08-23 18:42:44 -0400

    Init frontend

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ import Express from "express";
 
 const express = new Express();
 
-// Define Express route for GET requests with a UPC code in the path
-express.get("/", async (req, res) => {});
+express.use(Express.static("frontend"));
 
-// Start Express server
+// update current page color
+express.post("/api/color", async (req, res) => {});
+
+// get current page color
+express.get("/api/color", async (req, res) => {});
+
 express.listen(process.env["PORT"], () => {
   console.log(
     `Club Rush '24 API is now listening on port ${process.env["PORT"]}`
